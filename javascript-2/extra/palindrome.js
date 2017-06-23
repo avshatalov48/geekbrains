1c3efe4a775af61f01f2b6c35d267d3737664bc9
Alexander Shatalov <alexander.shataloff@gmail.com>
2017-06-23 20:58:04 +0300

    Update

---
+++ javascript-2/extra/palindrome.js
@@ -0,0 +1,11 @@
+// Решение в процессе, пока, что нет на него времени...
+
+function isPalindrome(s) {
+    var a = false,
+        m = s.toLocaleLowerCase(); // .split(/\d/); // вернет массив ["a", "b", "c", "d"].
+    return m;
+}
+
+console.log(isPalindrome("level")); // logs 'true'
+console.log(isPalindrome("levels")); // logs 'false'
+console.log(isPalindrome("A car, a man, a maraca")); // logs 'true'
