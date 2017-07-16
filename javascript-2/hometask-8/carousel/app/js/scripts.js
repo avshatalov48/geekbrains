9db563a4e9e332717d76da7b28951ed30ea38f84
Alexander Shatalov <alexander.shataloff@gmail.com>
2017-07-16 16:06:57 +0300

    GeekBrains > JS2 > Lesson 8 > Carousel Update

--- javascript-2/hometask-8/carousel/app/js/scripts.js
+++ javascript-2/hometask-8/carousel/app/js/scripts.js
@@ -1,173 +1,164 @@
-// Глобальные переменные
-var
-    nameElement = document.getElementById('form__name'),
-    birthdayElement = document.getElementById('form__birthday'),
-    phoneElement = document.getElementById('form__phone'),
-    emailElement = document.getElementById('form__email'),
-    textElement = document.getElementById('form__text'),
-    townElement = document.getElementById('form__town'),
-    check = false; // Переменная проверки правильности заполнения формы
-
 function Container() {
     this.id = '';
     this.className = '';
     this.htmlCode = '';
 }
 
-Container.prototype.render = function() {
+Container.prototype.render = function () {
     return this.htmlCode;
 };
 
-function Form(myId, myClass, myName, myBirthday, myPhone, myEmail, myText, myTown) {
-    Container.call(this);
-    this.id = myId;
-    this.className = myClass;
-    this.name = myName;
-    this.birthday = myBirthday;
-    this.phone = myPhone;
-    this.email = myEmail;
-    this.text = myText;
-    this.town = myTown;
+function Carousel() {
+    Container.call(this, "carousel");
+    this.countProducts = 0;
+    this.productsItems = [];
+
+    // количество отзывов
+    // this.countCarousel = 0;
+
+    // массив для хранения отзывов
+    // this.commentsItems = [];
+
+    // Получаем изначальные элементы из JSON
+    this.loadCarouselItems();
 }
 
-Form.prototype = Object.create(Container.prototype);
-Form.prototype.constructor = Form;
+Carousel.prototype = Object.create(Container.prototype);
+Carousel.prototype.constructor = Carousel;
 
-// Вставка значений в поля формы
-Form.prototype.fill = function() {
-    nameElement.value = this.name;
-    birthdayElement.value = this.birthday;
-    phoneElement.value = this.phone;
-    emailElement.value = this.email;
-    textElement.value = this.text;
-    townElement.value = this.town;
-};
-
-// Проверка полей формы на соответствие
-Form.prototype.validate = function(myIds, myTypes) {
-    for (var i in myTypes) {
-        var myType = myTypes[i],
-            myId = 'form__' + myType,
-            field = document.getElementById(myId),
-            message = document.getElementById('form__message-' + myType),
-            reg = /./;
-
-        switch (myType) {
-            case "name":
-                // Имя, минимум 2 буквы
-                reg = /^([A-zА-я]+){2}$/;
-                break;
-            case "birthday":
-                // Дата ДД.ММ.ГГГГ
-                reg = /^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/;
-                break;
-            case "phone":
-                // +7|8(000)000-0000
-                reg = /^\+[7|8]{1}\([0-9]{3}\)[0-9]{3}-[0-9]{4}$/;
-                break;
-            case "email":
-                // mymail@mail.ru, или my.mail@mail.ru, или my-mail@mail.ru
-                reg = /^([A-zА-я0-9_-]+\.)*[A-zА-я0-9_-]+@[A-zА-я0-9_-]+(\.[A-zА-я0-9_-]+)*\.[A-zА-я]{2,6}$/;
-                break;
-            case "town":
-                // Город
-                reg = /^([A-zА-я-\s]+){2}$/;
-                break;
-            case "text":
-                // Текст, минимум 10 символов
-                reg = /.{10}/;
-                break;
-        }
-
-        if (field.value.search(reg) == -1) {
-            check = false;
-            field.style.borderColor = '#c750ac';
-            $('#'+field.id).effect("bounce", { times: 3 }, "slow");
-            message.innerHTML = 'Проверьте правильность заполнения поля!';
-            $('#'+message.id).effect("bounce", { times: 3 }, "slow");
-        } else {
-            field.style.borderColor = '#85c799';
-            message.innerHTML = '';
-        }
-
-    }
-};
-
-// Загрузка информации в поля формы из файла JSON через AJAX
-document.getElementById('form__button-load').onclick = function() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', './json/data.json', true);
-    xhr.send();
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState != 4) return;
-
-        if (xhr.status != 200) {
-            console.log('Error', xhr.status, xhr.statusText);
-        } else {
-            var fileJson = JSON.parse(xhr.responseText),
-                formNew = new Form('formId', 'formClass',
-                fileJson.name,
-                fileJson.birthday,
-                fileJson.phone,
-                fileJson.email,
-                fileJson.text,
-                fileJson.town);
-            formNew.fill();
-        }
-    }
-};
-
-// Отслеживание события "submit" формы
-document.getElementById('form').onsubmit = function(e) {
-    check = true;
-    var formNew = new Form('formId', 'formClass',
-        nameElement.value,
-        birthdayElement.value,
-        phoneElement.value,
-        emailElement.value,
-        textElement.value,
-        townElement.value);
-    formNew.validate('form__input', ['name', 'birthday', 'phone', 'email', 'text', 'town']);
-    (!check) ? e.preventDefault() : alert('Форма успешно отправлена!');
-};
-
-// Загрузка списка городов из файла JSON в массив для автозаполнения через jQuery UI
-window.onload = function() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', './json/towns.json', true);
-    xhr.send();
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState != 4) return;
-
-        if (xhr.status != 200) {
-            console.log('Console: Error', xhr.status, xhr.statusText);
-        } else {
-            var myItems = JSON.parse(xhr.responseText);
-            $('#form__town').autocomplete({
-                source: myItems.town,
-                // Определяет минимальное число символов, ввод которых будет инициировать отображение раскрывающегося списка элементов автозаполнения. Значение по умолчанию — 1
-                minLength: 3
-            })
-
-        }
-    }
-
-    // Дата рождения
-    $.datepicker.setDefaults($.datepicker.regional["ru"]);
-    $('#form__birthday').datepicker({
-        dateFormat: 'dd.mm.yy',
-        dayNamesMin: ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'],
-        monthNames: [ 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь' ],
-        yearRange: '1900:2017',
-        changeYear: true,
-        firstDay: 1
+/*Carousel.prototype.render = function(root) {
+    var commentsDiv = $('<div />', {
+        id: this.id,
+        text: 'ОТЗЫВЫ:'
     });
 
-    // Маски для полей
-    // https://github.com/digitalBush/jquery.maskedinput
-    $("#form__birthday").mask('99.99.9999', {placeholder: 'дд.мм.гггг' });
-    $("#form__phone").mask('+7(999)999-9999', {placeholder: '+7(XXX)XXX-XXXX' });
+    var commentsItemsDiv = $('<div />', {
+        id: this.id + '_items'
+    });
+
+    commentsItemsDiv.appendTo(commentsDiv);
+    commentsDiv.appendTo(root);
+};*/
+
+// Получаем изначальные элементы из JSON
+Carousel.prototype.loadCarouselItems = function() {
+    // var appendId = '#' + this.id + '_items';
+
+    $.get({
+        url: './json/products.json',
+        dataType: 'json',
+        error: function() {
+            console.log ('JSON load: Error!');
+        },
+        success: function(data) {
+            console.log ('JSON load: Ок!');
+            // Количество отзывов
+            this.countProducts = data.products.length;
+            // Перебираем JSON и добавляем в массив
+            for (var item in data.products) {
+                this.productsItems.push(data.products[item]);
+            }
+            console.log (data, data.products.length, this.productsItems);
+        },
+        context: this
+    });
 
 };
+
+// Добавление отзыва
+/*Carousel.prototype.add = function (idComment, text, userMessage) {
+    var commentsItem = {
+        "id_comment": idComment,
+        "text": text,
+        "submit": false
+    };
+    // Увеличиваем количество отзывов
+    this.countCarousel++;
+    // Добавляем отзыв в массив
+    this.commentsItems.push(commentsItem);
+    // Обновляем данные
+    this.refresh();
+    if (!!userMessage) alert (userMessage);
+};*/
+
+// Удаление отзыва
+/*Carousel.prototype.del = function () {
+    if (this.countCarousel < 1) { this.refresh(); return; }
+    this.countCarousel--;
+    this.commentsItems.pop();
+    this.refresh();
+};*/
+
+// Одобрение отзыва
+/*Carousel.prototype.submit = function () {
+    this.commentsItems[this.countCarousel-1].submit = true;
+    this.refresh();
+};*/
+
+// Показать все отзывы
+/*Carousel.prototype.list = function () {
+    $('#comments__list').remove();
+
+    if (this.countCarousel < 1) { return; }
+
+    var commentsDiv = $('<div />', {
+        id: 'comments__list',
+        html: '<br><hr><br>СПИСОК ВСЕХ ОТЗЫВОВ:<br>&nbsp;'
+    });
+
+    for (var item in this.commentsItems) {
+        var comment = this.commentsItems[item],
+            commentsItemsDiv = $('<div />', {
+                html: '<hr width="50%"><p>Отзыв №' + comment.id_comment + '</p>'
+                + '<p>Текст: <span class="comments-text-list">' + comment.text + '</span></p>'
+                + '<p>Отзыв одобрен: ' + comment.submit + '</p>'
+            });
+        commentsItemsDiv.appendTo(commentsDiv);
+    }
+
+    commentsDiv.appendTo('body');
+};*/
+
+// Перерисовка
+/*Carousel.prototype.refresh = function() {
+    var commentsDataDiv = $('#comments_data');
+    commentsDataDiv.empty();
+    commentsDataDiv.append('<p>Всего отзывов: ' + this.countCarousel + '</p>');
+
+    // Вывод в консоль для самопроверки
+    console.log(this.commentsItems, this.countCarousel);
+
+    if (this.countCarousel < 1) {
+        $('.comments').hide();
+    } else {
+        $('.comments').show();
+    }
+};*/
+
+// Загрузка документа
+$(document).ready(function() {
+    var carousel = new Carousel();
+    // comments.render('#comments__wrapper');
+    //
+    // // Кнопка - Добавить
+    // $('.comments-add').on('click', function() {
+    //     var idComment = parseInt($(this).attr('id').split('_')[1]);
+    //     var text = $(this).parent().parent().find('.comments-text').val();
+    //     comments.add(idComment, text, 'Ваш отзыв был передан на модерацию!');
+    // });
+    //
+    // // Кнопка - Удалить
+    // $('.comments-delete').on('click', function() {
+    //     comments.del();
+    // });
+    //
+    // // Кнопка - Одобрить
+    // $('.comments-submit').on('click', function() {
+    //     comments.submit();
+    // });
+    //
+    // // Кнопка - Показать все отзывы
+    // $('.comments-list').on('click', function() {
+    //     comments.list();
+    // });
+});
