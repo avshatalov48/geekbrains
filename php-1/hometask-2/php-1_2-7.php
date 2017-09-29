2d627d4d34e0bf20ed6842140145222143d1fc41
Alexander Shatalov <alexander.shataloff@gmail.com>
2017-09-29 22:23:55 +0300

    GeekBrains > PHP-1 > Lesson 2 > Exercise 7 Complete

--- php-1/hometask-2/php-1_2-7.php
+++ php-1/hometask-2/php-1_2-7.php
@@ -6,59 +6,70 @@
  * 21 час 43 минуты
  */
 
-$h = rand(0, 23);
-$m = rand(0, 59);
-
-$randH = time() + (7 * $h * 60 * 60);
-$randM = time() + (7 * 24 * $m * 60);
-
-$hour = date("H", $randH);
-$minute = date("i", $randM);
-
-echo "$hour:$minute";
-
-/*Рабочий вариант "часов"*/
-switch ($hour) {
-    case 0:
-        $hourText = "часов";
-        break;
-    case ($hour == 1 || $hour == 21):
+/* Часы */
+function hourToString($hour)
+{
+    if ($hour == 1 || $hour == 21) {
         $hourText = "час";
-        break;
-    case ($hour == 2 || $hour == 3 || $hour == 4 || $hour == 22 || $hour == 23 || $hour == 24):
+    } elseif (($hour >= 2 && $hour <= 4) || ($hour >= 22 && $hour <= 24)) {
         $hourText = "часа";
-        break;
-    default:
+    } else {
         $hourText = "часов";
+    }
+    return $hourText;
 }
 
-/*$h = substr($hour, 1, 1);
-switch ($h) {
-    case ($h == 1 && $hour != 11 && $h != 0):
-        $hourText = "час";
-        break;
-    case (($h > 1 && $h < 5) || ($hour >= 22)):
-        $hourText = "часа";
-        break;
-    default:
-        $hourText = "часов";
-}*/
+/* Минуты */
+function minuteToString($minute)
+{
+    /* Алгоритм: 1, 21, 31, 41, 51 - всегда будет "минута".
+     * Если младший разряд больше 1-го и меньше 5, при этом само число при делении на 100
+     * имеет остаток больше 20 (то есть, например, не число 12 и не 112), то будут "минуты".
+     * В остальных случаях "минут".
+    */
 
-/*1, 21, 31, 41, 51 - всегда будет "минута". Если младший разряд больше 1-го и меньше 5, при этом само число при делении на 100 имеет остаток больше 20 (то есть, например, не число 12 и не 112), то будут "минуты". В остальных случаях "минут".*/
+    if (strlen($minute) < 2) {
+        $minute = "0" . $minute;
+    }
 
-$m = substr($minute, 1, 1);
-switch ($m) {
-    case 0:
-        $minuteText = "минут";
-        break;
-    case ($m == 1 && $minute != 11):
+    $m = substr($minute, 1, 1);
+
+    if ($m == 1 && $minute != 11) {
         $minuteText = "минута";
-        break;
-    case (($m > 1 && $m < 5) || ($minute % 100 > 20)):
+    } elseif (($minute > 1 && $minute < 5)
+        || (($m > 1 && $m < 5) && (($minute % 100) > 20))
+    ) {
         $minuteText = "минуты";
-        break;
-    default:
+    } else {
         $minuteText = "минут";
+    }
+
+    return $minuteText;
 }
 
-echo "<br>$hour $hourText $minute $minuteText";
+/* Итог: Часы + Минуты */
+function timeToString($hour, $minute)
+{
+    if ((!is_numeric($hour) || !is_numeric($minute))
+        || ($hour < 0 || $hour > 24) || ($minute < 0 || $minute > 60)
+    ) {
+        $result = "Неверные исходные данные";
+    } else {
+        $result = "$hour " . hourToString($hour) . " $minute " . minuteToString($minute);
+    }
+    return $result;
+}
+
+/* Тестирование */
+for ($i = 0; $i < 10; $i++) {
+    $h = rand(0, 23);
+    $m = rand(0, 59);
+
+    $randH = time() + (7 * $h * 60 * 60);
+    $randM = time() + (7 * 24 * $m * 60);
+
+    $hour = date("H", $randH);
+    $minute = date("i", $randM);
+    echo "Исходные данные: $hour:$minute<br>";
+    echo "Преобразованные:  " . timeToString($hour, $minute) . "<hr>";
+}
