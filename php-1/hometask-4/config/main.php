3c2d38aa658b9bfd887cd97715c378d8fc4ece6b
Alexander Shatalov <alexander.shataloff@gmail.com>
2017-10-06 21:07:37 +0300

    GeekBrains > PHP-1 > Lesson 4 > Exercise 1 Complete

--- php-1/hometask-4/config/main.php
+++ php-1/hometask-4/config/main.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "main.php";
+/*echo "Hello! I am main.php!";*/
+
+// define("PUBLIC_DIR", $_SERVER['DOCUMENT_ROOT'] . "/");
+define("PUBLIC_DIR", "./");
+define("ENGINE_DIR", PUBLIC_DIR . "engine/");
+define("IMAGES_DIR", PUBLIC_DIR . "img/");
+define("ROOT_DIR", PUBLIC_DIR . "../");
+define("CONFIG_DIR", ROOT_DIR . "config/");
+define("UPLOADS_DIR", ROOT_DIR . "uploads/");
