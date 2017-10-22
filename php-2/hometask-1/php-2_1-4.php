2a142b7f2fe26c4fd47fda22530b5ad411913759
Alexander Shatalov <alexander.shataloff@gmail.com>
2017-10-22 11:30:10 +0300

    GeekBrains > PHP-2 > Lesson 1 > Exercise 1-4 Complete

--- php-2/hometask-1/php-2_1-4.php
+++ php-2/hometask-1/php-2_1-4.php
@@ -27,7 +27,7 @@ class Product
     public function __construct($article, $category, $title, $description, $size, $weight, $price, $guarantee, $country, $count)
     {
         $this->article = $article;
-        $this->id_category = $category;
+        $this->category = $category;
         $this->title = $title;
         $this->description = $description;
         $this->size = $size;
@@ -41,8 +41,9 @@ class Product
     public function view()
     {
         echo "
+            <hr><h2>Карточка товара</h2>
             <b>Артикул:</b> $this->article<br>
-            <b>Категория:</b> $this->id_category<br>
+            <b>Категория:</b> $this->category<br>
             <b>Наименование:</b> $this->title<br>
             <b>Описание:</b> $this->description<br>
             <b>Размер:</b> $this->size<br>
@@ -50,30 +51,74 @@ class Product
             <b>Цена:</b> $this->price руб.<br>
             <b>Гарантия:</b> $this->guarantee мес.<br>
             <b>Страна-производитель:</b> $this->country<br>
-            <b>Количество на складе:</b> $this->count<br>
+            <b>Количество на складе:</b> $this->count шт.<br>
         ";
     }
 
     // Списание товара со склада
     public function removeFromStock($number)
     {
-        echo "<hr>";
+        echo "<hr><h2>Списание со склада</h2>";
         if (($this->count - $number) < 0) {
-            echo "<b>Недостаточное количество товара на складе для списания!</b><br>";
+            echo "<b>Недостаточное количество товара на складе для списания: $number шт.!</b><br>";
         } else {
             $this->count -= $number;
-            echo "<b>Списание товара $this->title в количестве $number выполнено успешно!</b><br>";
+            echo "<b>Списание товара $this->title в количестве $number шт. выполнено успешно!</b><br>";
         }
-        echo "<b>Остаток на складе:</b> $this->count<br>";
+        echo "<b>Остаток на складе:</b> $this->count шт.<br>";
     }
 
 }
 
-$good1 = new Product("#326166", "Неттопы", "Lenovo ThinkCentre M710q",
+// Уцененый товар (брак, не комплект)
+class Discount extends Product
+{
+    public $state;
+    public $complete;
+    public $functionality;
+    public $package;
+    public $reason;
+
+    function __construct($article, $category, $title, $description, $size, $weight, $price, $guarantee, $country, $count,
+                         $state, $complete, $functionality, $package, $reason)
+    {
+        parent::__construct($article, $category, $title, $description, $size, $weight, $price, $guarantee, $country, $count);
+        $this->state = $state;
+        $this->complete = $complete;
+        $this->functionality = $functionality;
+        $this->package = $package;
+        $this->reason = $reason;
+    }
+
+    public function view()
+    {
+        parent::view();
+        echo "
+            <b>Состояние:</b> $this->state<br>
+            <b>Комплектность:</b> $this->complete<br>
+            <b>Функциональность:</b> $this->functionality<br>
+            <b>Состояние упаковки:</b> $this->package<br>
+            <b>Причина уценки:</b> $this->reason<br>
+        ";
+    }
+}
+
+/*Стандартный товар*/
+$good1 = new Product(326166, "Неттопы", "Lenovo ThinkCentre M710q",
     "ПЭВМ Lenovo ThinkCentre M710q <10MRS04600> Intel Core i3-7100T 4ГБ RAM 128Гб SSD DOS",
     "49.99 x 27.84 x 15.25 см", 3.55, 28087, 12, "Китай", "7");
 $good1->view();
 $good1->removeFromStock(10);
 $good1->removeFromStock(2);
 
-var_dump(good1);
+/*Уцененный товар*/
+$good2 = new Discount(4142497, "Ноутбуки", "MSI GT72 2QE-1488RU Dominator Pro, 9S7-178131-1488",
+    "MSI GT72 2QE-1488RU Dominator Pro, 9S7-178131-1488, 17.3\" (1920x1080), 12GB, 1000GB, Intel Core i7-5700HQ(2.7), DVD±RW DL, 4GB NVIDIA GeForce GTX980M, LAN, WiFi, Bluetooth, Win8.1; Отремонтированный",
+    "428 x 294 x 48", 3.78, 82830, 2, "Китай", 2,
+    "незначительные следы эксплуатации (мелкие потертости, царапины)", "полная", "полностью исправен", "отличное", "отремонтированный");
+$good2->view();
+$good2->removeFromStock(3);
+$good2->removeFromStock(1);
+
+var_dump($good1);
+var_dump($good2);
