b48caea8c8a691e01b237d5ad44bf609524939ad
Alexander Shatalov <alexander.shataloff@gmail.com>
2017-10-22 15:09:42 +0300

    GeekBrains > PHP-2 > Lesson 1 > Exercise 6 Complete

--- php-2/hometask-1/php-2_5.php
+++ php-2/hometask-1/php-2_5.php
@@ -2,45 +2,46 @@
 
 /**
  * 5. Дан код:
-
-class A {
-public function foo() {
-static $x = 0;
-echo ++$x;
-}
-}
-$a1 = new A();
-$a2 = new A();
-$a1->foo();
-$a2->foo();
-$a1->foo();
-$a2->foo();
-Что он выведет на каждом шаге? Почему?
-Немного изменим п.5
-
-class A {
-public function foo() {
-static $x = 0;
-echo ++$x;
-}
-}
-class B extends A {
-}
-$a1 = new A();
-$b1 = new B();
-$a1->foo();
-$b1->foo();
-$a1->foo();
-$b1->foo();
-
+ *
+ * class A {
+ * public function foo() {
+ * static $x = 0;
+ * echo ++$x;
+ * }
+ * }
+ * $a1 = new A();
+ * $a2 = new A();
+ * $a1->foo();
+ * $a2->foo();
+ * $a1->foo();
+ * $a2->foo();
+ * Что он выведет на каждом шаге? Почему?
+ * Немного изменим п.5
+ *
+ * class A {
+ * public function foo() {
+ * static $x = 0;
+ * echo ++$x;
+ * }
+ * }
+ * class B extends A {
+ * }
+ * $a1 = new A();
+ * $b1 = new B();
+ * $a1->foo();
+ * $b1->foo();
+ * $a1->foo();
+ * $b1->foo();
  */
-
-class A {
-    public function foo() {
+class A
+{
+    public function foo()
+    {
         static $x = 0;
         echo ++$x . '<br>';
     }
 }
+
 $a1 = new A();
 $a2 = new A();
 $a1->foo();
@@ -52,14 +53,19 @@ $a2->foo();
 echo '<hr>';
 
 
-class C {
-    public function foo() {
+class C
+{
+    public function foo()
+    {
         static $x = 0;
         echo ++$x . '<br>';
     }
 }
-class B extends C {
+
+class B extends C
+{
 }
+
 $c1 = new C();
 $b1 = new B();
 $c1->foo();
@@ -74,7 +80,7 @@ echo '<hr><pre>
 Наследование класса (и метода) приводит к тому, что всё-таки создается новый метод.
 
 Вывод: динамические методы в PHP существуют в контексте классов, а не объектов.
-И только лишь в рантайме происходит подстановка "$this = текущий_объект"
+И только лишь в рантайме происходит подстановка "$this = текущий_объект".
 
 Источник: <a href="https://habrahabr.ru/post/259627/">Готовимся к собеседованию по PHP: ключевое слово "static"</a>
 </pre>';
