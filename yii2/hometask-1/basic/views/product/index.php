bf1e6a7e01af96f21cd0401a6bb1863f38dcada6
Alexander Shatalov <alexander.shataloff@gmail.com>
2017-12-24 19:50:42 +0300

    GeekBrains > Yii2 > Lesson 1 > In process

--- yii2/hometask-1/basic/views/product/index.php
+++ yii2/hometask-1/basic/views/product/index.php
@@ -1,2 +1,17 @@
-<h1><?=$title?></h1>
-<p><?=$content?></p>
+<?php
+
+use yii\helpers\Html;
+
+$this->title = $title;
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="product">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+	<p><?=$short_description?></p>
+	<img src="<?=$photo?>" class="img-responsive center-block">
+	<?=$description?>
+	<h3>Цена:</h3><?=$price?>
+
+</div>
