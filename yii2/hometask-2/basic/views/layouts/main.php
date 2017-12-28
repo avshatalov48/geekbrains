7e4cb59c828685475e56f549e37608d0ff8d4341
Alexander Shatalov <alexander.shataloff@gmail.com>
2017-12-28 22:13:37 +0300

    GeekBrains > Yii2 > Lesson 2 > Process

--- yii2/hometask-2/basic/views/layouts/main.php
+++ yii2/hometask-2/basic/views/layouts/main.php
@@ -41,6 +41,8 @@ AppAsset::register($this);
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Products', 'url' => ['/product/index']],
+            ['label' => 'Admin', 'url' => ['/admin/index']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
